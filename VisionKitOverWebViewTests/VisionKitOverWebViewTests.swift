6d29addcf36743af578cc42408d939881b2263bf
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-05 04:13:21 +0500

    alignment

--- VisionKitOverWebViewTests/VisionKitOverWebViewTests.swift
+++ VisionKitOverWebViewTests/VisionKitOverWebViewTests.swift
@@ -2,16 +2,16 @@ import XCTest
 @testable import VisionKitOverWebView
 
 class VisionKitOverWebViewTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
     }
-
+    
     override func tearDownWithError() throws {
     }
-
+    
     func testExample() throws {
     }
-
+    
     func testPerformanceExample() throws {
         self.measure {
         }
